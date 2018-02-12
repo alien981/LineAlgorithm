b90c1a099409236a0c055f5e9dd2670af917544b
alien981 <dsparakis@stuy.edu>
2018-02-12 02:21:10 -0500

    added other octants

--- draw.py
+++ draw.py
@@ -1,7 +1,20 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+	if x0=x1:
+		y = y0
+		while y<y1:
+			plot(x0,y)
+			y +=1
+	elif y0=y1:
+		x=x0
+		while x<x1:
+			plot(x,y0)
+			x+=1
+    else:
+		if x0<0:
+			if y0>x0:
+				#insert some way to split it
 
 def drawOct1(x0, y0, x1, y1, screen, color):
 	x = x0
@@ -42,6 +55,15 @@ def drawOct3(x0, y0, x1, y1, screen, color):
 	A = x0-x1
 	B = y0-y1
 	C = y0*(x0-x1) - x0*(y0-y1)
+	d = -2*A +B
+			
+	while x>-250:
+		plot(x,y)
+		if d>0:
+			y+=1
+			d +=B
+		x-=1
+		d -= 2 * A
 
 def drawOct4(x0, y0, x1, y1, screen, color):
 	x = x0
@@ -49,6 +71,15 @@ def drawOct4(x0, y0, x1, y1, screen, color):
 	A = x0-x1
 	B = y0-y1
 	C = y0*(x0-x1) - x0*(y0-y1)
+	d = -A + 2*B
+	
+	while y<250:
+		plot(x,y)
+		if d>0:
+			x-=1
+			d -= 2*A
+		y+=1
+		d -= 2*B
 
 def drawOct5(x0, y0, x1, y1, screen, color):
 	x = x0
@@ -56,6 +87,15 @@ def drawOct5(x0, y0, x1, y1, screen, color):
 	A = x0-x1
 	B = y0-y1
 	C = y0*(x0-x1) - x0*(y0-y1)
+	d = -2*A - B
+	
+	while x>-250:
+		plot(x,y)
+		if d>0:
+			y-=1
+			d+=2*B
+		x+=1
+		d+=2*A
 
 def drawOct6(x0, y0, x1, y1, screen, color):
 	x = x0
@@ -63,6 +103,15 @@ def drawOct6(x0, y0, x1, y1, screen, color):
 	A = x0-x1
 	B = y0-y1
 	C = y0*(x0-x1) - x0*(y0-y1)
+	d = -2*A -B
+			
+	while x>-250:
+		plot(x,y)
+		if d>0:
+			y-=1
+			d -=B
+		x-=1
+		d -= 2 * A
 
 def drawOct7(x0, y0, x1, y1, screen, color):
 	x = x0
@@ -70,6 +119,15 @@ def drawOct7(x0, y0, x1, y1, screen, color):
 	A = x0-x1
 	B = y0-y1
 	C = y0*(x0-x1) - x0*(y0-y1)
+	d = 2*A -B
+			
+	while x>-250:
+		plot(x,y)
+		if d>0:
+			y-=1
+			d -=B
+		x+=1
+		d += 2 * A
 
 
 def drawOct8(x0, y0, x1, y1, screen, color):
