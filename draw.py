975d6ee459d67ede1de95df170c30281099652a2
alien981 <dsparakis@stuy.edu>
2018-02-15 11:43:03 -0500

    made it work

--- draw.py
+++ draw.py
@@ -1,54 +1,56 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-	if x0=x1:
+	if x0 == x1 :
 		y = y0
 		while y<y1:
-			plot(x0,y)
-			y +=1
-	elif y0=y1:
+				plot(screen, color, x0, y)
+				y +=1
+	elif y0==y1:
 		x=x0
 		while x<x1:
-			plot(x,y0)
+			plot(screen, color, x,y0)
 			x+=1
-    else:
-		if x0<0:
-			if y0>x0:
-				#insert some way to split it
+	else:
+		if ( abs(x1-x0) >= abs(y1 - y0) ):
+			drawOct1(x0, y0, x1, y1, screen, color)
+		else:
+			drawOct2(x0, y0, x1, y1, screen, color)
+
+				
 
 def drawOct1(x0, y0, x1, y1, screen, color):
 	x = x0
 	y = y0
-	A = x0-x1
-	B = y0-y1
-	C = y0*(x0-x1) - x0*(y0-y1)
+	A = 2 * (y1 - y0)
+	B = -2 * (x1 - x0)
 	d = 2*A + B
-	
-	while x<250:
-		plot(x,y)
+	while x<x1:
+		plot(screen, color, x, y)
 		if d>0:
 			y+=1
-			d +=B
-		x+=1
+			d += 2 * B
+		x +=1
 		d += 2 * A
+
 	
 	
 def drawOct2(x0, y0, x1, y1, screen, color):
 	x = x0
 	y = y0
-	A = x0-x1
-	B = y0-y1
-	C = y0*(x0-x1) - x0*(y0-y1)
+	A = 2 * (y1 - y0)
+	B = -2 * (x1 - x0)
 	d = A + 2*B
 	
-	while y<250:
-		plot(x,y)
+	while y<y1:
+		plot(screen, color, x,y)
 		if d<0:
 			x+=1
 			d += 2*A
 		y+=1
 		d += 2*B
-		
+
+'''		
 def drawOct3(x0, y0, x1, y1, screen, color):
 	x = x0
 	y = y0
@@ -145,5 +147,5 @@ def drawOct8(x0, y0, x1, y1, screen, color):
 			d+=2*B
 		x+=1
 		d+=2*A
-		
+		'''
 		
--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
+c1 = [255, 255, 255]
+c2 = [255, 0, 0]
+c3 = [0, 0, 255]
+
+draw_line(200, 100, 200, 200, screen, color) 
+draw_line(0, 100, 100, 100, screen, color) 
+draw_line(0, 0, 500, 100, screen, c3)
+draw_line(0, 0, 100, 500, screen, c2)
 
 display(screen)
 save_extension(screen, 'img.png')
